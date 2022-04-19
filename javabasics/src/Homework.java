9e5ddb49c13937b1fd7786151b7fe481ba54f5f6
igorsiwek <igor.siwek94@gmail.com>
2022-04-20 00:37:18 +0200

    add homework

---
+++ javabasics/src/Homework.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+
+        if (age > 18) {
+            System.out.println("jest ok");
+        } else {
+            System.out.println("jest niedobrze");
+
+        }
+    }
+}
