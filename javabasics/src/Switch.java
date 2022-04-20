cfa858afbf8425df937a2ee4147bb0f14a8cc9d4
igorsiwek <igor.siwek94@gmail.com>
2022-04-20 20:52:16 +0200

    class 20.04.2022

---
+++ javabasics/src/Switch.java
@@ -0,0 +1,29 @@
+public class Switch {
+    public static void main(String[] args) {
+        int a = 11;
+        switch (a){
+            case 1:
+                System.out.println("poniedzialek");
+                break;
+            case 2:
+                System.out.println("wtorek");
+                break;
+            case 3:
+                System.out.println("sroda");
+                break;
+            case 4:
+                System.out.println("czwartek");
+                break;
+            case 5:
+                System.out.println("piatek");
+                break;
+            case 6:
+                System.out.println("sobota");
+                break;
+            default:
+                System.out.println("tatata");
+
+
+        }
+    }
+}
